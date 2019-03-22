8a302e5158982ef41ac95242a0fb5b2600e3ceac
pooya parsa <pooya@pi0.ir>
2019-03-22 13:53:54 +0430

    example: use youtube demo

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -9,5 +9,7 @@ module.exports = {
   },
   modules: [
     { handler: require('../') }
-  ]
+  ],
+  vuetify: {
+  }
 }
--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,11 +1,14 @@
 <template>
-<div>
-    Works!
-</div>
+  <v-container fill-height>
+    <v-layout justify-center align-center>
+      <v-flex shrink>
+        <v-tooltip right>
+          <v-btn icon large href="" target="_blank" slot="activator">
+            <v-icon large>code</v-icon>
+          </v-btn>
+          <span>Source</span>
+        </v-tooltip>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
-
-<script>
-export default {
-
-}
-</script>
