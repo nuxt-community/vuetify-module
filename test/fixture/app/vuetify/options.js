78495a36cd0f884805fe946cc89e68b6dcf2895b
Kevin Marrec <kevin@marrec.io>
2019-07-31 00:58:24 +0200

    feat: vuetifyOptions through options file (#86)

---
+++ test/fixture/app/vuetify/options.js
@@ -0,0 +1,12 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: colors.red.base
+      }
+    }
+  }
+}
