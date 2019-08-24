71c63f56decd33ba6046b89f4118a71ed65e89d8
Dmitry Molotkov <aldarund@gmail.com>
2019-08-24 19:43:00 +0300

    feat: manual import of components (#125)
    * feat: manual import - initial state (#110)
    
    * feat: manual import
    
    * fix: treeshake false situation

---
+++ test/fixture/pages/manual-import.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <!-- eslint-disable-next-line vue/require-component-is -->
+        <component is="v-chip">
+          Default
+        </component>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
