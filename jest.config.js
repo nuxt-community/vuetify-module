b9c56f52ec908bdf0157301aaaf4e2a22d3fe64a
Kevin Marrec <kevin@marrec.io>
2019-08-30 17:54:06 +0200

    chore: add coverage 100% threshold

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,13 @@ module.exports = {
   collectCoverageFrom: [
     'lib/**/*.js',
     '!lib/templates/**'
-  ]
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 }
