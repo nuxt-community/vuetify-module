e8de2c65085229c160c9876c3ece2a802d025aa4
Kevin Marrec <kevin@marrec.io>
2020-01-13 23:51:14 +0100

    fix: update devDependencies & fix code

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,7 +3,13 @@ import { ModuleThis } from '@nuxt/types/config/module'
 import { Options } from './options'
 
 export default function setupPlugin (this: ModuleThis, options: Options) {
-  this.options.build!.transpile!.push('vuetify/lib')
+  const transpile = this.options.build!.transpile!
+
+  if (Array.isArray(transpile)) {
+    transpile.push('vuetify/lib')
+  } else {
+    this.options.build!.transpile = ctx => [...transpile(ctx), 'vuetify/lib']
+  }
 
   const optionsPath = typeof options.frameworkOptions === 'string' && this.nuxt.resolver.resolveAlias(options.frameworkOptions)
 
